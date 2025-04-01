414fe6a377d1adf21a096182d73ca05f4fddc45c
Alejandra Ramoz Velez <anavelez@192.168.80.25>
2025-04-01 15:59:10 -0500

    feat: add CI workflow

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 function updateMessage() {
-    const versionInput = document.getElementById('versionInput').value;
-    const messageElement = document.getElementById('message');
+  // Obtener el ID de commit desde una variable global
+  const commitId = window.commitId; // Esto debe estar definido en el flujo de CI/Backend
+
+  const messageElement = document.getElementById('message');
   
-    if (versionInput) {
-      messageElement.textContent = `Bienvenidos al mundo GitOps v${versionInput}`;
-    } else {
-      messageElement.textContent = 'Bienvenidos al mundo GitOps v0.0.0';
-    }
+  if (commitId) {
+      messageElement.textContent = `Bienvenidos al mundo GitOps (Commit ID: ${commitId})`;
+  } else {
+      messageElement.textContent = 'Bienvenidos al mundo GitOps (Commit ID no disponible)';
   }
+}
