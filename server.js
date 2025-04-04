6d38257b3660fbeddc68f9d96bdff90fc008809b
Alejandra Ramos <alramo@wenia.com>
2025-04-04 08:31:53 -0500

    add new env vars

--- server.js
+++ server.js
@@ -3,10 +3,24 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send('¡Bienvenidos al mundo GitOps! 🚀');
-});
+const APP_NAME = process.env.APP_NAME || "Default App";
+const WELCOME_MESSAGE = process.env.WELCOME_MESSAGE || "Bienvenido!";
+const FOOTER_TEXT = process.env.FOOTER_TEXT || "Powered by GitOps";
 
-app.listen(PORT, () => {
+app.get("/", (req, res) => {
+    res.send(`
+      <html>
+        <head>
+          <title>${APP_NAME}</title>
+        </head>
+        <body>
+          <h1>${WELCOME_MESSAGE}</h1>
+          <footer>${FOOTER_TEXT}</footer>
+        </body>
+      </html>
+    `);
+  });
+  
+  app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
-});
+  });
