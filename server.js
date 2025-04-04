26f7b6767324d3090a65d07e5e6de9e983eacd16
Alejandra Ramos <alramo@wenia.com>
2025-04-04 11:16:59 -0500

    add new env secrets

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const PORT = process.env.PORT || 3000;
 const APP_NAME = process.env.APP_NAME || "Default App";
 const WELCOME_MESSAGE = process.env.WELCOME_MESSAGE || "Bienvenido!";
 const FOOTER_TEXT = process.env.FOOTER_TEXT || "Powered by GitOps";
+const SECRET1 = process.env.SECRET1 || "No disponible";
+const SECRET2 = process.env.SECRET2 || "No disponible";
 
 app.get("/", (req, res) => {
     res.send(`
@@ -15,6 +17,8 @@ app.get("/", (req, res) => {
         </head>
         <body>
           <h1>${WELCOME_MESSAGE}</h1>
+          <p><strong>Secret1:</strong> ${SECRET1}</p>
+          <p><strong>Secret2:</strong> ${SECRET2}</p>
           <footer>${FOOTER_TEXT}</footer>
         </body>
       </html>
